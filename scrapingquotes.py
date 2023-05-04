747e04a3c81119be83f8802e18ed5da6d27527af
heymmhr <hemanmaharjan10@gmail.com>
2023-05-04 16:54:46 +0545

    saving the data in csv file

--- scrapingquotes.py
+++ scrapingquotes.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 quote_url = 'http://quotes.toscrape.com/'
 
@@ -10,6 +11,13 @@ soup = BeautifulSoup(response.content, 'html.parser')
 
 quotes = soup.find_all('span', {'class':'text'})
 authors = soup.find_all('small', {'class' : 'author'})
-print(authors)
+
+file = open("scraped_quotes.csv","wt")
+writer = csv.writer(file)
+
+writer.writerow(["Quotes","Authors"])
+
 for quote,author in zip(quotes,authors) :
     print(quote.text +" - " + author.text)
+    writer.writerow([quote.text, author.text])
+file.close()
